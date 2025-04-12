0467f49259895e9c0051ae8998df260ff5017392
hsnshehata <hsn.shehata@gmail.com>
2025-04-12 10:50:10 +0200

    Update Rule.js

--- server/models/Rule.js
+++ server/models/Rule.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 
 const ruleSchema = new mongoose.Schema({
   botId: { type: mongoose.Schema.Types.ObjectId, ref: 'Bot', required: true },
-  type: { type: String, enum: ['global', 'general', 'products', 'qa'], required: true },
-  content: {
-    type: mongoose.Schema.Types.Mixed,
-    required: true,
-  },
-  createdAt: { type: Date, default: Date.now },
+  type: { type: String, required: true, enum: ['general', 'products', 'qa', 'global'] },
+  content: { type: mongoose.Schema.Types.Mixed, required: true },
 });
 
 module.exports = mongoose.model('Rule', ruleSchema);
