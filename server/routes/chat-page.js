66bbf30ecf8c947ff1467ef928f73b8d49494228
hsnshehata <hsn.shehata@gmail.com>
2025-04-18 08:32:58 +0200

    Update chat-page.js

--- server/routes/chat-page.js
+++ server/routes/chat-page.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const chatPageController = require('../controllers/chatPageController');
 
-// Create a new chat page
 router.post('/', chatPageController.createChatPage);
+router.put('/:id', chatPageController.updateChatPage);
 
 module.exports = router;
