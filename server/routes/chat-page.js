008d4919642062ab23dd0e041516ed603cf6f8f7
hsnshehata <hsn.shehata@gmail.com>
2025-04-18 09:06:06 +0200

    Update chat-page.js

--- server/routes/chat-page.js
+++ server/routes/chat-page.js
@@ -5,5 +5,6 @@ const chatPageController = require('../controllers/chatPageController');
 router.post('/', chatPageController.createChatPage);
 router.put('/:id', chatPageController.updateChatPage);
 router.get('/:linkId', chatPageController.getChatPageByLinkId);
+router.get('/bot/:botId', chatPageController.getChatPageByBotId);
 
 module.exports = router;
