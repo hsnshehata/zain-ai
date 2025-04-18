7664ced9b8993833ac4b1599fe77e3afac684872
hsnshehata <hsn.shehata@gmail.com>
2025-04-18 10:09:48 +0200

    Delete public/js/whatsapp.js

--- public/js/whatsapp.js
+++
@@ -1,7 +0,0 @@
-function loadWhatsAppPage() {
-  const content = document.getElementById('content');
-  content.innerHTML = `
-    <h2>واتساب</h2>
-    <p>سيتم إضافة دعم واتساب قريبًا...</p>
-  `;
-}
