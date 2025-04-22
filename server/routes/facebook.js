8b05d5814a80433b4a0549845f71313d46d86d52
hsnshehata <hsn.shehata@gmail.com>
2025-04-22 04:28:21 +0200

    Update facebook.js

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { handleMessage } = require('../controllers/facebookController');
+const { getSettings, updateSettings } = require('../controllers/botController');
+const authMiddleware = require('../middleware/auth'); // افتراض وجود middleware للتوثيق
 
-// Webhook لاستقبال الرسائل من فيسبوك
 router.post('/', handleMessage);
+
+// Routes لإعدادات التصاريح
+router.get('/settings', authMiddleware, getSettings);
+router.patch('/settings', authMiddleware, updateSettings);
 
 module.exports = router;
