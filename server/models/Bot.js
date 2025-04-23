3fe18bf0be675b331547c813beafc57030f0862b
hsnshehata <hsn.shehata@gmail.com>
2025-04-23 09:30:18 +0200

    Update Bot.js

--- server/models/Bot.js
+++ server/models/Bot.js
@@ -11,7 +11,10 @@ const botSchema = new mongoose.Schema({
   messagingReferralsEnabled: { type: Boolean, default: false },
   messageEditsEnabled: { type: Boolean, default: false },
   inboxLabelsEnabled: { type: Boolean, default: false },
-  sendCartEnabled: { type: Boolean, default: false },
+  workingHours: {
+    start: { type: String, default: '09:00' }, // ساعة البداية (بتوقيت 24 ساعة)
+    end: { type: String, default: '17:00' },   // ساعة النهاية (بتوقيت 24 ساعة)
+  },
 });
 
 module.exports = mongoose.model('Bot', botSchema);
