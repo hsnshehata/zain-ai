3774a866d3ad448a638629a1d77f0500bb5ed986
hsnshehata <hsn.shehata@gmail.com>
2025-05-05 09:48:23 +0300

    Update notifications.js

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { sendGlobalNotification, getNotifications, markAsRead } = require('../controllers/notificationsController');
-const auth = require('../middleware/auth');
+const authenticate = require('../middleware/authenticate');
 
-router.post('/global', auth, sendGlobalNotification);
-router.get('/', auth, getNotifications);
-router.put('/:id/read', auth, markAsRead);
+router.post('/global', authenticate, sendGlobalNotification);
+router.get('/', authenticate, getNotifications);
+router.put('/:id/read', authenticate, markAsRead);
 
 module.exports = router;
