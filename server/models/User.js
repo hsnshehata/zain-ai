054f35ef171b5da6cc8ba14e71274c2cac4d9c9f
hsnshehata <hsn.shehata@gmail.com>
2025-05-06 07:10:19 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   role: { type: String, enum: ['user', 'superadmin'], default: 'user' },
   bots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bot' }],
   createdAt: { type: Date, default: Date.now },
-  email: { type: String, unique: true, required: false }, // غيرنا required لـ false
-  whatsapp: { type: String },
-  googleId: { type: String }
+  email: { type: String, unique: true, required: true },
+  whatsapp: { type: String, required: true },
+  googleId: { type: String },
+  isVerified: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
