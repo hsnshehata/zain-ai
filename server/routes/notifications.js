740647dae96e082cfabe3b24d8344a215a6cd140
hsnshehata <hsn.shehata@gmail.com>
2025-05-08 06:41:14 +0300

    Update notifications.js

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const { sendGlobalNotification, getNotifications, markAsRead } = require('../controllers/notificationsController');
 const authenticate = require('../middleware/authenticate');
 
+// روت لإرسال إشعار عام لكل المستخدمين (للسوبر أدمن بس)
 router.post('/global', authenticate, sendGlobalNotification);
+
+// روت لجلب إشعارات المستخدم الحالي
 router.get('/', authenticate, getNotifications);
+
+// روت لتعليم إشعار كمقروء
 router.put('/:id/read', authenticate, markAsRead);
 
 module.exports = router;
