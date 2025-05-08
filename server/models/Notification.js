ac564770bf9433c60cb57007b69bd34d48e03167
hsnshehata <hsn.shehata@gmail.com>
2025-05-08 06:28:30 +0300

    Update Notification.js

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
+  title: { type: String, required: true }, // حقل جديد لعنوان الإشعار
   message: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   isRead: { type: Boolean, default: false },
