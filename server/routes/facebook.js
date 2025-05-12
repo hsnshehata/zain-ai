d70727e20cd27c35583c4b9ace14e508c3f05c50
hsnshehata <hsn.shehata@gmail.com>
2025-05-12 11:45:37 +0300

    Update facebook.js

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -1,15 +1,10 @@
-// /server/routes/facebook.js
-
 const express = require('express');
 const router = express.Router();
-const facebookController = require('../controllers/facebookController');
-const facebookTokenController = require('../controllers/facebookTokenController');
-const authenticate = require('../middleware/authenticate');
+const { getSettings, updateSettings } = require('../controllers/botController');
+const authMiddleware = require('../middleware/authenticate');
 
-// Webhook endpoint
-router.post('/', facebookController.handleMessage);
-
-// New endpoint to exchange short-lived token for long-lived token
-router.post('/exchange-token', authenticate, facebookTokenController.exchangeToken);
+// Routes for settings with botId in the URL
+router.get('/:id/settings', authMiddleware, getSettings);
+router.patch('/:id/settings', authMiddleware, updateSettings);
 
 module.exports = router;
