61c00d37ff570f22eb7d6dfad4b889a70fb1e7d9
hsnshehata <hsn.shehata@gmail.com>
2025-05-16 06:21:13 +0300

    Update webhook.js

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { handleMessage } = require('../controllers/facebookController');
+const { handleMessage: handleFacebookMessage } = require('../controllers/facebookController');
+const { handleMessage: handleInstagramMessage } = require('../controllers/instagramController');
 
 router.get('/facebook', (req, res) => {
   const VERIFY_TOKEN = 'hassanshehata';
@@ -11,10 +12,10 @@ router.get('/facebook', (req, res) => {
 
   if (mode && token) {
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('✅ Webhook verified');
+      console.log('✅ Facebook Webhook verified');
       res.status(200).send(challenge);
     } else {
-      console.log('❌ Webhook verification failed');
+      console.log('❌ Facebook Webhook verification failed');
       res.sendStatus(403);
     }
   } else {
@@ -22,6 +23,28 @@ router.get('/facebook', (req, res) => {
   }
 });
 
-router.post('/facebook', handleMessage);
+router.post('/facebook', handleFacebookMessage);
+
+router.get('/instagram', (req, res) => {
+  const VERIFY_TOKEN = 'hassanshehata';
+
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('✅ Instagram Webhook verified');
+      res.status(200).send(challenge);
+    } else {
+      console.log('❌ Instagram Webhook verification failed');
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.post('/instagram', handleInstagramMessage);
 
 module.exports = router;
