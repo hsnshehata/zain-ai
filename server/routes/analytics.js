cab82b4c88814342983449bd9e10b464545deb7b
hsnshehata <hsn.shehata@gmail.com>
2025-06-01 00:36:53 +0300

    Delete server directory

--- server/routes/analytics.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const analyticsController = require('../controllers/analyticsController');
-
-// Get analytics for a specific bot
-router.get('/', analyticsController.getAnalytics);
-
-module.exports = router;
