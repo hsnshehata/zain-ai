402ffe5df9a84f0116cac6f4b3dd7291f01a12a8
hsnshehata <hsn.shehata@gmail.com>
2025-06-06 04:55:57 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,15 +6,16 @@ const userSchema = new mongoose.Schema({
     type: String, 
     required: true, 
     unique: true, 
-    lowercase: true, // تحويل الـ username للحروف الصغيرة تلقائيًا
-    trim: true // إزالة أي مسافات زيادة
+    lowercase: true,
+    trim: true,
+    match: /^[a-z0-9_-]+$/ // شرط إن الـ username يكون حروف إنجليزي، أرقام، _ أو - فقط
   },
   password: { type: String, required: false },
   role: { type: String, enum: ['user', 'superadmin'], default: 'user' },
   bots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bot' }],
   createdAt: { type: Date, default: Date.now },
   email: { type: String, unique: true, required: true },
-  whatsapp: { type: String, required: true },
+  whatsapp: { type: String, required: false }, // غيرناه لاختياري
   googleId: { type: String },
   isVerified: { type: Boolean, default: false },
   subscriptionType: { type: String, enum: ['free', 'monthly', 'yearly'], default: 'free' },
